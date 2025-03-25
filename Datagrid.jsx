57c51ba7e9d532eec5960d36244851d708c74ddf
Bhuvan Chandra Mothe <152079724+BhuvanChandraMothe@users.noreply.github.com>
2025-03-25 17:49:17 +0530

    Update Datagrid.jsx

--- Datagrid.jsx
+++ Datagrid.jsx
@@ -7,57 +7,27 @@ export default class DataGridDemo extends React.Component {
     constructor() {
         super();
         this.state = {
-            rows: [
-                { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-                { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-                { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-                { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-                { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-                { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-                { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-                { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-                { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-            ],
-            columns: [
-                { field: 'id', headerName: 'ID', width: 90 },
-                {
-                    field: 'firstName',
-                    headerName: 'First name',
-                    width: 150,
-                    editable: true,
-                },
-                {
-                    field: 'lastName',
-                    headerName: 'Last name',
-                    width: 150,
-                    editable: true,
-                },
-                {
-                    field: 'age',
-                    headerName: 'Age',
-                    type: 'number',
-                    width: 110,
-                    editable: true,
-                },
-                {
-                    field: 'fullName',
-                    headerName: 'Full name',
-                    description: 'This column has a value getter and is not sortable.',
-                    sortable: false,
-                    width: 160,
-                    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-                },
-            ]
-            
-
+            rows: [ ],
+            columns: [ ]
         }
+    }
+    async componentDidMount() {
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let data = await response.json();
+        this.setState({rows : await data});
+        
+        Object.keys(this.state.rows[0]).map((propName) => 
+            this.setState((prevState) => ({
+                columns: [...prevState.columns, {field: propName, headerName: propName.toLocaleUpperCase(), width: 150, editable: true},],
+            })));
+
     }
     render(){
         const {rows, columns} = this.state;
         return (
             <Box sx={{ height: 400, width: '100%' }}>
                 <DataGrid
-                    rows={rows}
+                    rows={this.state.rows}
                     columns={columns}
                     initialState={{
                         pagination: {
